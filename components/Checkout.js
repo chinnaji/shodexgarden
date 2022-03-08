92e7647de846f3da5c52788a3914d10d29a30d61
Chibuike Nnaji <67190735+Max1583@users.noreply.github.com>
2022-03-08 12:29:05 -0800

    changed class to className in checkout page

--- components/Checkout.js
+++ components/Checkout.js
@@ -33,15 +33,15 @@ function Checkout() {
           placeholder="Phone Number"
           required
         /> */}
-        <div class="form-check">
+        <div className="form-check">
           <input
-            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-lime-500 checked:border-lime-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-lime-500 checked:border-lime-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
             type="checkbox"
             value=""
             id="flexCheckDefault"
           />
           <label
-            class="form-check-label inline-block text-gray-800"
+            className="form-check-label inline-block text-gray-800"
             for="flexCheckDefault"
           >
             Default checkbox
