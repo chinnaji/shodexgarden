452e253d83f0f1b5899dabd1d25b1ded62fc2954
Chibuike Nnaji <67190735+Max1583@users.noreply.github.com>
2022-04-01 21:19:50 -0800

    still testing

--- pages/api/tickets.js
+++ pages/api/tickets.js
@@ -10,35 +10,4 @@ export default async function getAllTickets(req, res) {
     // .sort({ published: -1 })
     .toArray();
   res.status(200).json(tickets);
-
-  // res.status(200).json([
-  //   {
-  //     id: "01",
-  //     title: "Swimming Ticket (Adults)",
-  //     image: "/images/tickets (2).jpg",
-  //     price: "3500",
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: "02",
-  //     title: "Swimming Ticket (Kids)",
-  //     image: "/images/tickets (1).jpg",
-  //     price: "1200",
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: "03",
-  //     title: "Garden Area Ticket (Adults)",
-  //     image: "/images/tickets (3).jpg",
-  //     price: "2500",
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: "04",
-  //     title: "Garden Area Ticket (Kids)",
-  //     image: "/images/tickets (4).jpg",
-  //     price: "1500",
-  //     quantity: 1,
-  //   },
-  // ]);
 }
--- pages/tickets.js
+++ pages/tickets.js
@@ -12,7 +12,6 @@ function tickets({ ticketsFromDb }) {
 
   return (
     <>
-      {process.env.NODE_ENV}
       <main>
         <TicketsListing ticketItems={ticketsFromDb} />
       </main>
@@ -24,9 +23,9 @@ function tickets({ ticketsFromDb }) {
 // export async function getStaticProps() {
 export async function getStaticProps() {
   const res = await fetch(
-    // process.env.NODE_ENV == "development"
-    //   ? "http://localhost:3000/api/tickets":
-    "https://shodexgarden.vercel.app/api/tickets"
+    process.env.NODE_ENV == "development"
+      ? "http://localhost:3000/api/tickets"
+      : "https://shodexgarden.vercel.app/api/tickets"
   );
   const ticketsFromDb = await res.json();
   return {
