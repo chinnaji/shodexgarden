061c1e04e68d16d3daf8045c43f132b763f5bbdb
Chibuike Nnaji <67190735+Max1583@users.noreply.github.com>
2022-04-02 11:00:18 -0800

    UPDATE HELLO.JS

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,33 +4,37 @@ var md5 = require("md5");
 export default async function handler(req, res) {
   // console.log(md5('message'));
 
-  try {
-    var hashedTicketId = md5(new Date().toISOString().slice(0, 19));
+  // try {
+  //   var hashedTicketId = md5(new Date().toISOString().slice(0, 19));
 
-    // async function saveToDb() {
-    const client = await clientPromise;
-    const db = client.db("shodexGarden");
-    // const data = await db.collection("shodexGardenOrders").find({}).toArray();
+  //   // async function saveToDb() {
+  //   const client = await clientPromise;
+  //   const db = client.db("shodexGarden");
+  //   // const data = await db.collection("shodexGardenOrders").find({}).toArray();
 
-    const orderDetails = {
-      id: new Date().toISOString().slice(0, 19),
-      ticketId: hashedTicketId,
-      datePurchased: new Date().toISOString(),
-      isValid: true,
-      orderItems: ["apple", "orange", "mango"],
-      total: "10000",
-      customerDetails: ["apple", "orange", "mango"],
-    };
-    await db.collection("shodexGardenOrders").insertOne(orderDetails);
-    return res.json({
-      message: "order added successfully",
-      success: true,
-    });
-  } catch (error) {
-    // return an error
-    return res.json({
-      message: new Error(error).message,
-      success: false,
-    });
-  }
+  //   const orderDetails = {
+  //     id: new Date().toISOString().slice(0, 19),
+  //     ticketId: hashedTicketId,
+  //     datePurchased: new Date().toISOString(),
+  //     isValid: true,
+  //     orderItems: ["apple", "orange", "mango"],
+  //     total: "10000",
+  //     customerDetails: ["apple", "orange", "mango"],
+  //   };
+  //   await db.collection("shodexGardenOrders").insertOne(orderDetails);
+  //   return res.json({
+  //     message: "order added successfully",
+  //     success: true,
+  //   });
+  // } catch (error) {
+  //   // return an error
+  //   return res.json({
+  //     message: new Error(error).message,
+  //     success: false,
+  //   });
+  // }
+  return res.json({
+    message: process.env.NULI,
+    success: true,
+  });
 }
